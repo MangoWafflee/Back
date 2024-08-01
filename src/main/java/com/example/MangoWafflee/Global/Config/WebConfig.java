5f8f204478cdd4ca45c127fda6140cd6e2436680
tjdals4716 <song47166@naver.com>
2024-08-01 15:14:15 +0900

    :white_check_mark: 프론트 허용 도메인 추가

--- src/main/java/com/example/MangoWafflee/Global/Config/WebConfig.java
+++ src/main/java/com/example/MangoWafflee/Global/Config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "https://mango.angrak.cloud")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "https://mango.angrak.cloud", "https://hahasmile.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
